d19dd58c54efa7fd1d73bf4b8f7f89f9715ab590
zakzakst <59270506+zakzakst@users.noreply.github.com>
2026-04-11 10:28:20 +0900

    Initial commit

---
+++ eslint.config.mjs
@@ -0,0 +1,13 @@
+import nextVitals from "eslint-config-next/core-web-vitals";
+import nextTs from "eslint-config-next/typescript";
+import storybook from "eslint-plugin-storybook";
+import { defineConfig, globalIgnores } from "eslint/config";
+
+const eslintConfig = defineConfig([
+  ...nextVitals,
+  ...nextTs,
+  globalIgnores([".next/**", "out/**", "build/**", "next-env.d.ts"]),
+  ...storybook.configs["flat/recommended"],
+]);
+
+export default eslintConfig;
