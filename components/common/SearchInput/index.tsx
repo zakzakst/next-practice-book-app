bc7023f7dcb1984407ad08c9d6d128a8e3a05361
zakzakst <e1433104@gmail.com>
2026-04-13 20:35:47 +0900

    fix

--- components/common/SearchInput/index.tsx
+++ components/common/SearchInput/index.tsx
@@ -1,3 +1,12 @@
-export const SearchInput = () => {
+"use client";
+
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+
+type Props = {
+  onSubmit: (text: string) => void;
+};
+
+export const SearchInput = ({ onSubmit }: Props) => {
   return <div>SearchInput</div>;
 };
