70402aa216b35e44979e5f232139c4930776abbd
zakzakst <e1433104@gmail.com>
2026-04-17 04:53:53 +0900

    お気に入り登録・解除API作成

---
+++ dummy-db/favorite.ts
@@ -0,0 +1,32 @@
+import { Favorite } from "@/types/domain/favorite";
+
+export const favorites: Favorite[] = [
+  {
+    id: 1,
+    bookId: 1,
+    userId: 1,
+    createdAt: "2024-01-15T10:00:00.000Z",
+    updatedAt: "2024-01-15T10:00:00.000Z",
+  },
+  {
+    id: 2,
+    bookId: 3,
+    userId: 1,
+    createdAt: "2024-01-15T10:00:00.000Z",
+    updatedAt: "2024-01-15T10:00:00.000Z",
+  },
+  {
+    id: 3,
+    bookId: 2,
+    userId: 2,
+    createdAt: "2024-01-15T10:00:00.000Z",
+    updatedAt: "2024-01-15T10:00:00.000Z",
+  },
+  {
+    id: 4,
+    bookId: 3,
+    userId: 2,
+    createdAt: "2024-01-15T10:00:00.000Z",
+    updatedAt: "2024-01-15T10:00:00.000Z",
+  },
+];
