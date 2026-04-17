7177046702dbdbffb9c9c000648969b8398a96ad
zakzakst <e1433104@gmail.com>
2026-04-17 18:37:39 +0900

    レビューAPI作成

---
+++ app/api/reviews/[id]/route.ts
@@ -0,0 +1,104 @@
+import { NextRequest, NextResponse } from "next/server";
+
+import { reviews } from "@/dummy-db/review";
+import { users } from "@/dummy-db/user";
+import { apiDelay } from "@/lib/api";
+import { getJwtPayload } from "@/lib/jwt";
+import { UpdateReviewRequest, UpdateReviewResponse } from "@/types/api/reviews";
+
+// TODO: エラーレスポンスのルール考える
+type Error = {
+  error: string;
+};
+
+export const PUT = async (
+  request: NextRequest,
+  context: RouteContext<"/api/reviews/[id]">,
+): Promise<NextResponse<UpdateReviewResponse | Error>> => {
+  await apiDelay();
+
+  try {
+    // レビューが存在するか確認
+    const { id } = await context.params;
+    const review = reviews.find((r) => r.id === Number(id));
+    if (!review) {
+      return NextResponse.json(
+        { error: "指定されたレビューが見つかりません" },
+        { status: 404 },
+      );
+    }
+
+    // 自身が投稿したレビューか確認
+    const jwtPayload = await getJwtPayload();
+    const user = users.find((u) => u.id === jwtPayload?.id);
+    if (!user || review.userId !== user.id) {
+      return NextResponse.json(
+        { error: "処理の実行権限がありません" },
+        { status: 403 },
+      );
+    }
+
+    const params: UpdateReviewRequest = await request.json();
+    const updatedReview = {
+      ...review,
+      ...params,
+      updatedAt: new Date().toISOString(),
+    };
+    const index = reviews.findIndex((r) => r === review);
+    reviews[index] = updatedReview;
+
+    return NextResponse.json({
+      ...updatedReview,
+      user: {
+        ...user,
+        // TODO: 調べる。ユーザーが出る度にpassword消す処理するの不便
+        password: undefined,
+      },
+    });
+  } catch {
+    return NextResponse.json(
+      { error: "サーバーのエラーが発生しました" },
+      { status: 500 },
+    );
+  }
+};
+
+export const DELETE = async (
+  _request: NextRequest,
+  context: RouteContext<"/api/reviews/[id]">,
+): Promise<NextResponse<null | Error>> => {
+  await apiDelay();
+
+  try {
+    // レビューが存在するか確認
+    const { id } = await context.params;
+    const review = reviews.find((r) => r.id === Number(id));
+    if (!review) {
+      return NextResponse.json(
+        { error: "指定されたレビューが見つかりません" },
+        { status: 404 },
+      );
+    }
+
+    // 自身が投稿したレビューか確認
+    const jwtPayload = await getJwtPayload();
+    const user = users.find((u) => u.id === jwtPayload?.id);
+    if (!user || review.userId !== user.id) {
+      return NextResponse.json(
+        { error: "処理の実行権限がありません" },
+        { status: 403 },
+      );
+    }
+
+    const index = reviews.findIndex((r) => r === review);
+    reviews.splice(index, 1);
+
+    // TODO: 調べる。DELETEで何も返さない時の書き方忘れた
+    return NextResponse.json(null);
+  } catch {
+    return NextResponse.json(
+      { error: "サーバーのエラーが発生しました" },
+      { status: 500 },
+    );
+  }
+};
--- dummy-db/review.ts
+++ dummy-db/review.ts
@@ -1,3 +1,40 @@
 import { Review } from "@/types/domain/review";
 
-export const review: Review[] = [];
+export const reviews: Review[] = [
+  {
+    id: 1,
+    bookId: 1,
+    userId: 1,
+    rating: 5,
+    comment: "とても面白かったです！！",
+    createdAt: "2024-01-15T10:00:00.000Z",
+    updatedAt: "2024-01-15T10:00:00.000Z",
+  },
+  {
+    id: 2,
+    bookId: 2,
+    userId: 1,
+    rating: 4,
+    comment: "すごく読みやすかったです",
+    createdAt: "2024-03-15T10:00:00.000Z",
+    updatedAt: "2024-03-15T10:00:00.000Z",
+  },
+  {
+    id: 3,
+    bookId: 1,
+    userId: 2,
+    rating: 3,
+    comment: "登場人物に共感できました",
+    createdAt: "2024-10-15T10:00:00.000Z",
+    updatedAt: "2024-10-15T10:00:00.000Z",
+  },
+  {
+    id: 4,
+    bookId: 2,
+    userId: 2,
+    rating: 4.5,
+    comment: "やっと読み終わりました。楽しい読書体験ができました",
+    createdAt: "2025-01-15T10:00:00.000Z",
+    updatedAt: "2025-01-15T10:00:00.000Z",
+  },
+];
