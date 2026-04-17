bde7403e782c5a9a4361f9f5296b9fb2703ea402
zakzakst <e1433104@gmail.com>
2026-04-17 13:00:15 +0900

    storybook修正

--- stories/features/books/BookDetail.stories.tsx
+++ stories/features/books/BookDetail.stories.tsx
@@ -1,12 +1,33 @@
 import { BookDetail } from "@/components/features/books/BookDetail";
+import { AuthProvider } from "@/contexts/AuthContext";
 import { books } from "@/dummy-db/book";
+import { authMeHandler } from "@/lib/msw/handlers/auth";
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { fn } from "storybook/test";
 
 const meta = {
   title: "Features/Books/BookDetail",
   component: BookDetail,
   // TODO: テンプレートのほうにも反映（experimental削除）
   tags: ["autodocs"],
+  args: {
+    onUpdateFavorite: fn(),
+  },
+  parameters: {
+    msw: {
+      handlers: [authMeHandler],
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  decorators: [
+    (Story) => (
+      <AuthProvider>
+        <Story />
+      </AuthProvider>
+    ),
+  ],
 } satisfies Meta<typeof BookDetail>;
 
 export default meta;
@@ -14,12 +35,24 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    book: books[1],
+    book: {
+      ...books[1],
+      favorite: {
+        count: 0,
+        state: false,
+      },
+    },
   },
 };
 
 export const NoImage: Story = {
   args: {
-    book: books[0],
+    book: {
+      ...books[0],
+      favorite: {
+        count: 0,
+        state: false,
+      },
+    },
   },
 };
--- stories/features/books/BooksList.stories.tsx
+++ stories/features/books/BooksList.stories.tsx
@@ -1,13 +1,40 @@
 import { BooksList } from "@/components/features/books/BooksList";
+import { AuthProvider } from "@/contexts/AuthContext";
 import { books } from "@/dummy-db/book";
+import { authMeHandler } from "@/lib/msw/handlers/auth";
 import type { Meta, StoryObj } from "@storybook/nextjs-vite";
+import { fn } from "storybook/test";
 
-// import { fn } from "storybook/test";
+const frontBooks = books.map((b) => ({
+  ...b,
+  favorite: {
+    count: 0,
+    state: false,
+  },
+}));
 
 const meta = {
   title: "Features/Books/BooksList",
   component: BooksList,
   tags: ["autodocs"],
+  args: {
+    onUpdateFavorite: fn(),
+  },
+  parameters: {
+    msw: {
+      handlers: [authMeHandler],
+    },
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  decorators: [
+    (Story) => (
+      <AuthProvider>
+        <Story />
+      </AuthProvider>
+    ),
+  ],
 } satisfies Meta<typeof BooksList>;
 
 export default meta;
@@ -15,7 +42,8 @@ type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
   args: {
-    items: books,
+    // TODO: 全ての書籍が表示されるので、数を制限する
+    items: frontBooks,
   },
 };
 
